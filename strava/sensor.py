eb4e095d302a4ff710e665c90ea6eda00c430184
Magnus Larsson <maglars2@gmail.com>
2019-05-02 15:34:08 +0200

    Add version tag

--- strava/sensor.py
+++ strava/sensor.py
@@ -76,6 +76,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         })])
 })
 
+VERSION = '0.0.1'
+
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Strava sensors."""
