a38b1e151b308d73a99ad5ebc4b11f1070e663ca
Magnus Larsson <maglars2@gmail.com>
2019-06-12 13:33:50 +0200

    Update sensor to use callbacks

--- strava/src/online/sensor.py
+++ strava/src/online/sensor.py
@@ -2,15 +2,21 @@ import asyncio
 
 from datetime import datetime
 from datetime import timedelta
+from aiohttp import web
 import logging
+import json
+import os
 
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_RESOURCES, CONF_TYPE, ATTR_ATTRIBUTION
+from homeassistant.components.http import HomeAssistantView
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import callback
 from homeassistant.helpers.event import async_call_later
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.typing import HomeAssistantType
 from homeassistant.util import Throttle
 
 REQUIREMENTS = ['stravalib==0.10.2']
@@ -67,8 +73,16 @@ TOTALS_TYPES = {
     ATTR_TOTAL_MOVING_TIME: [lambda hass: '', lambda value, hass: value[ATTR_TOTAL_MOVING_TIME]]
 }
 
+CONF_CLIENT_ID = 'client_id'
+CONF_CLIENT_SECRET = 'client_secret'
+
+STRAVA_CONFIG_PATH = 'strava.json'
+
+DATA_CALLBACK = 'strava-callback'
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ACCESS_TOKEN): cv.string,
+    vol.Required(CONF_CLIENT_ID): cv.string,
+    vol.Required(CONF_CLIENT_SECRET): cv.string,
     vol.Optional(CONF_RESOURCES, default={CONF_TYPE: ATTR_BIGGEST_RIDE_DISTANCE, CONF_ARG: ATTR_TOTAL_DISTANCE}):
         vol.All(cv.ensure_list, [vol.Schema({
             vol.Required(CONF_TYPE): vol.In(SENSOR_TYPES),
@@ -81,30 +95,112 @@ VERSION = '0.0.1'
 @asyncio.coroutine
 def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up the Strava sensors."""
-    devices = []
-    for resource in config.get(CONF_RESOURCES):
-        sensor_type = resource[CONF_TYPE]
-        is_simple_sensor = len(SENSOR_TYPES[sensor_type]) == 4
-        if CONF_ARG not in resource:
-            resource[CONF_ARG] = ATTR_TOTAL_DISTANCE
+    from stravalib import Client
+    client = Client()
 
-        name = SENSOR_TYPES[sensor_type][0]
-        icon = SENSOR_TYPES[sensor_type][1]
-        if is_simple_sensor:
-            unit_of_measurement_lambda = SENSOR_TYPES[sensor_type][2]
-            state_transformer_lambda = SENSOR_TYPES[sensor_type][3]
+    @asyncio.coroutine
+    def _read_config():
+        if not os.path.isfile(config_path):
+            return None
+        with open(config_path, 'r') as auth_file:
+            config = json.load(auth_file)
+            if config['client_id'] == client_id:
+                return config['access_token']
+
+    @asyncio.coroutine
+    def _write_config(access_token):
+        with open(config_path, 'w') as auth_file:
+            json.dump({
+                'client_id': client_id,
+                'access_token': access_token,
+            }, auth_file)
+
+    @asyncio.coroutine
+    def _add_devices(access_token):
+        devices = []
+        for resource in config.get(CONF_RESOURCES):
+            sensor_type = resource[CONF_TYPE]
+            is_simple_sensor = len(SENSOR_TYPES[sensor_type]) == 4
+            if CONF_ARG not in resource:
+                resource[CONF_ARG] = ATTR_TOTAL_DISTANCE
+
+            name = SENSOR_TYPES[sensor_type][0]
+            icon = SENSOR_TYPES[sensor_type][1]
+            if is_simple_sensor:
+                unit_of_measurement_lambda = SENSOR_TYPES[sensor_type][2]
+                state_transformer_lambda = SENSOR_TYPES[sensor_type][3]
+            else:
+                total_type = resource[CONF_ARG]
+                unit_of_measurement_lambda = TOTALS_TYPES[total_type][0]
+                state_transformer_lambda = TOTALS_TYPES[total_type][1]
+
+            devices.append(StravaSensor(hass=hass, sensor_type=sensor_type, name=name, icon=icon, unit_of_measurement_lambda=unit_of_measurement_lambda, state_transformer_lambda=state_transformer_lambda))
+
+        strava_data = StravaData(hass, access_token, devices)
+        yield from strava_data.fetch_data()
+        return async_add_devices(devices)
+
+    @asyncio.coroutine
+    def initialize_callback(code):
+        """Handle OAuth callback from Strava authorization flow."""
+        access_token = client.exchange_code_for_token(
+            client_id=client_id, client_secret=client_secret, code=code)
+        yield from _write_config(access_token)
+        yield from _add_devices(access_token)
+        configurator.async_request_done(request_id)
+
+    hass.http.register_view(StravaAuthCallbackView())
+
+    client_id = config.get(CONF_CLIENT_ID)
+    client_secret = config.get(CONF_CLIENT_SECRET)
+
+    config_path = hass.config.path(STRAVA_CONFIG_PATH)
+
+    access_token = yield from _read_config()
+    if access_token is not None:
+        yield from _add_devices(access_token)
+    else:
+        callback_url = '{}{}'.format(
+            hass.config.api.base_url, StravaAuthCallbackView.url)
+        authorize_url = client.authorization_url(
+            client_id=client_id, redirect_uri=callback_url)
+
+        configurator = hass.components.configurator
+        request_id = configurator.async_request_config(
+            'Strava',
+            description='Authorization required for Strava account.',
+            link_name='Authorize Home Assistant',
+            link_url=authorize_url,
+            entity_picture='/local/icons/logo_strava.png')
+
+    hass.data[DATA_CALLBACK] = initialize_callback
+    return True
+
+class StravaAuthCallbackView(HomeAssistantView):
+    """Web view that handles OAuth authentication and redirection flow."""
+    requires_auth = False
+    url = '/api/strava/callback'
+    name = 'api:strava:callback'
+
+    @callback
+    def get(self, request):  # pylint: disable=no-self-use
+        """Handle browser HTTP request."""
+        hass = request.app['hass']
+        params = request.query
+        response = web.HTTPFound('/states')
+
+        if 'code' not in params:
+            _LOGGER.error(
+                'Error authorizing Strava: %s',
+                params.get('error', 'invalid response'))
+        elif DATA_CALLBACK not in hass.data:
+            _LOGGER.error('Configuration request not found')
         else:
-            total_type = resource[CONF_ARG]
-            unit_of_measurement_lambda = TOTALS_TYPES[total_type][0]
-            state_transformer_lambda = TOTALS_TYPES[total_type][1]
+            code = params['code']
+            initialize_callback = hass.data[DATA_CALLBACK]
+            hass.async_add_job(initialize_callback(code))
 
-        devices.append(StravaSensor(hass=hass, sensor_type=sensor_type, name=name, icon=icon, unit_of_measurement_lambda=unit_of_measurement_lambda, state_transformer_lambda=state_transformer_lambda))
-    async_add_devices(devices)
-
-    accesstoken = config.get(CONF_ACCESS_TOKEN)
-    strava_data = StravaData(hass, accesstoken, devices)
-
-    yield from strava_data.fetch_data()
+        return response
 
 class StravaSensor(Entity):
     """Representation of a Strava sensor."""
@@ -122,12 +218,10 @@ class StravaSensor(Entity):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return self._name
 
     @property
     def state(self):
-        """Return the state of the device."""
         if not self._state:
             return None
 
@@ -135,17 +229,14 @@ class StravaSensor(Entity):
 
     @property
     def should_poll(self):
-        """No polling needed."""
         return False
 
     @property
     def icon(self):
-        """Return the icon for the frontend."""
         return self._icon
 
     @property
     def device_state_attributes(self):
-        """Return other details about the sensor state."""
         attr = {
             ATTR_ATTRIBUTION: CONF_ATTRIBUTION,
         }
@@ -157,7 +248,6 @@ class StravaSensor(Entity):
 
     @property
     def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
         return self._unit_of_measurement(self.hass)
 
     def set_state(self, new_state):
@@ -167,7 +257,6 @@ class StravaData:
     """Implementation of the Strava data retriever."""
 
     def __init__(self, hass, accesstoken, devices):
-        """Initialize the sensor."""
         self.hass = hass
 
         from stravalib import Client
@@ -214,4 +303,3 @@ class StravaData:
 
         if tasks:
             yield from asyncio.wait(tasks, loop=self.hass.loop)
-
